25f98588f434af4e231228afc5ef1e2ec61b7b6d
kangjian99 <radiokj@163.com>
2023-06-14 17:01:25 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ import os
 import openai
 
 model = 'gpt-3.5-turbo' # or text-davinci-003
+model_16k = 'gpt-3.5-turbo-16k'
+
 SESSION_SECRET_KEY = os.environ.get('SESSION_SECRET_KEY')
 
 server = 'tcp:kj99.database.windows.net,1433'
@@ -11,7 +13,5 @@ db_password = os.environ.get('DB_PASSWORD')
 driver = '{ODBC Driver 18 for SQL Server}'
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
-openai_api_key = os.environ.get('OPENAI_API_KEY')
-API_URL = "https://api.openai.com/v1/chat/completions"
 
 directory = 'session_messages'
