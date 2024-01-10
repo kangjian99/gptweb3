bd7c5ec89b1a1669a4074842e4b5cf9977a3f3b4
kangjian99 <radiokj@163.com>
2024-01-10 11:10:04 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
 import os
-import openai
+from openai import OpenAI
 
+client = OpenAI(
+  api_key = os.environ.get('OPENAI_API_KEY'),
+)
+  
 model = 'gpt-3.5-turbo-1106' # or text-davinci-003
 model_4 = 'gpt-4-1106-preview'
 
@@ -12,6 +16,6 @@ db_username = os.environ.get('DB_USERNAME')
 db_password = os.environ.get('DB_PASSWORD')
 driver = '{ODBC Driver 18 for SQL Server}'
 
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+#openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 directory = 'session_messages'
